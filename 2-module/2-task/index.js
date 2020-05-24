9872bf834237d8f30ea66c0a5a5034f19871a54f
Mikhail Kushkov <mikhailkushkov@Misha-pro.fritz.box>
2020-05-24 17:41:43 +0200

    module2 - tasks

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -4,4 +4,17 @@
  * @returns {Boolean}
  */
 function isEmpty(obj) {
+  if (Object.entries(obj).length === 0) {
+    return true
+  } else {
+    return false
+  }
 }
+
+let schedule = {};
+
+console.log( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "подъём";
+
+console.log( isEmpty(schedule) ); // false
